6d4c11e7e59d0ac3bdab290b60e4599a73df930b
Goodluck Okechukwu <ebubeokechukwu13@gmail.com>
2026-03-24 14:58:52 +0100

    Posting and fetching of produce is completed

--- backend/app/routes/produce.py
+++ backend/app/routes/produce.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify, request
-from app.database.db_functions import save_produce_details, get_current_user
+from app.database.db_functions import save_produce_details, get_current_user, update_farmer_position, get_all_produce
 
 produce_bp = Blueprint("produce_bp", __name__, url_prefix="/api/v1")
+
 
 @produce_bp.route("/produce", methods=['POST'])
 def addProduce():
@@ -11,7 +12,7 @@ def addProduce():
         return jsonify({"status": "ERROR",
                         "code": 401,
                         "message": "Unauthorized. Please log in."}), 401
-    
+
     data = request.json.get("produceDetails", {})
     if not data:
         return jsonify({"status": "ERROR",
@@ -28,14 +29,48 @@ def addProduce():
         return jsonify({"status": "ERROR",
                         "code": 400,
                         "message": "Incomplete data provided for produce upload."})
-    
-    # result = save_produce_details(farmer_id, crop_name, pickup_location, destination, quantity, details)
 
     location_data = request.json.get("farmerLocation")
     if not location_data:
-        return jsonify({"status": "ERROR", 
+        return jsonify({"status": "ERROR",
                         "code": 400,
                         "message": "Farmer location not provided."})
-    
+
     farm_lat = location_data.get("lat", "")
     farm_lng = location_data.get("lng", "")
+
+    if not all([farm_lat, farm_lng]):
+        return jsonify({"status": "ERROR",
+                        "code": 400,
+                        "message": "Farm location details are incomplete."})
+
+    result = update_farmer_position(farmer_id, farm_lat, farm_lng)
+
+    if result["code"] == 200:
+        result2 = save_produce_details(
+            farmer_id, crop_name, pickup_location, destination, quantity, details)
+        return jsonify(result2), result2['code']
+
+    return jsonify(result), result['code']
+
+
+@produce_bp.route("/produce", methods=['GET'])
+def fetch_produce():
+    farmer_sid = request.cookies.get('farmer_session_id')
+    user_id = get_current_user(farmer_sid, "farmer")
+    role = "farmer"
+
+    # 2. If not a farmer, try to get the driver
+    if not user_id:
+        driver_sid = request.cookies.get('driver_session_id')
+        user_id = get_current_user(driver_sid, "driver")
+        role = "driver"
+
+    if not user_id:
+        return jsonify({"status": "ERROR",
+                        "code": 401,
+                        "message": "User is unauthorized."})
+
+    result = get_all_produce()
+
+    return jsonify(result), result['code']
