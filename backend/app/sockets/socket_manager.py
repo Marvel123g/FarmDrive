a55ea505b6626a5ff1d2930eaf0649f301ab32e8
Marvel123g <marvellousolorunwa23@gmail.com>
2026-03-28 06:06:06 +0100

    updated url

--- backend/app/sockets/socket_manager.py
+++ backend/app/sockets/socket_manager.py
@@ -1,4 +1,5 @@
 from flask_socketio import SocketIO
 
 # We initialize without the app first
-socketio = SocketIO(cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(cors_allowed_origins="*", async_mode='eventlet',
+    supports_credentials=True)
